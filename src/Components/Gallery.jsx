d4933f934d80890ded311466c1ef837a0f1de621
shriti19 <shritichanda9@gmail.com>
2024-09-03 14:18:40 +0530

    changed gallery.jsx

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 const Gallery = () => {
-  // const gallery=[
-  //   "./img1.webp",
-  //   "./img2.jpg",
-  //   "./img3.jpg",
-  //   "./img4.jpg",
-  //   "./img5.jpg",
-  //   "./img6.jpg",
-  //   "./img7.jpg",
-  //   "./img8.jpg",
-  // ];
+  const gallery=[
+    "./img1.webp",
+    "./img2.jpg",
+    "./img3.jpg",
+    "./img4.jpg",
+    "./img5.jpg",
+    "./img6.jpg",
+    "./img7.jpg",
+    "./img8.jpg",
+  ];
   return (
     <section className='gallery'>
-      {/* <h1>BETTER BEATS BEST</h1>
+      <h1>BETTER BEATS BEST</h1>
       <div className="images">
         <div>
           {
@@ -22,7 +22,7 @@ const Gallery = () => {
             ))
           }
         </div>
-      </div> */}
+      </div>
         
     </section>
   )
