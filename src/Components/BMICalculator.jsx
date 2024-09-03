a74cb0642786f96eaeb3320af16577532852116a
shriti19 <shritichanda9@gmail.com>
2024-09-03 13:56:44 +0530

    added files

---
+++ src/Components/BMICalculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BMICalculator = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default BMICalculator
---
+++ src/Components/WorkoutSessions.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const WorkoutSessions = () => {
+  return (
+    <section className='workout_session'>
+      {/* <div className="wrapper">
+      <h1>TOP WORKOUT SESSION</h1>
+      <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus consectetur nisi fugiat quae ipsa sapiente exercitationem illo et.</P>
+      <img src="./img5.jpg" alt="workout" />
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consequatur officiis inventore porro laborum pariatur cupiditate animi error?</P>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi consequatur modi, cupiditate eveniet at velit.</p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi consequatur modi, cupiditate eveniet at velit.</p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi consequatur modi, cupiditate eveniet at velit.</p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi consequatur modi, cupiditate eveniet at velit.</p>
+          </div>
+        </div>
+      </div>
+         */}
+    </section>
+  )
+}
+
+export default WorkoutSessions
